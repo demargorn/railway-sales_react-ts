c54a5871c1114c262b6e3aecb620587c2e12253e
Михаил Дорошин <demargorn@gmail.com>
2025-03-14 01:57:29 +0300

    adaptive added

--- src/widgets/Main/Main.tsx
+++ src/widgets/Main/Main.tsx
@@ -7,16 +7,20 @@ const Main = () => {
          <h1 className={styles['logo']}>Railway Sales</h1>
          <nav className={styles['navbar']}>
             <div className={styles['navbar__content']}>
-               <Link to='/about' className={styles['navbar__link']}>
+               <Link to='/' className={styles['navbar__link']}>
                   О нас
                </Link>
-               <Link to='/how' className={styles['navbar__link']}>
+               <Link to='/' className={styles['navbar__link']}>
                   Как это работает
                </Link>
-               <Link to='/reviews' className={styles['navbar__link']}>
+               <Link
+                  to='/'
+                  onClick={() => window.scrollTo(0, 800)}
+                  className={styles['navbar__link']}
+               >
                   Отзывы
                </Link>
-               <Link to='/contacts' className={styles['navbar__link']}>
+               <Link to='/' className={styles['navbar__link']}>
                   Контакты
                </Link>
             </div>
@@ -24,6 +28,46 @@ const Main = () => {
          <h1 className={styles['main-title']}>
             Вся жизнь - <span className={styles['main-title__slogan']}>путешествие!</span>
          </h1>
+
+         <form className={styles['search-form']}>
+            <div className={styles['search-form__direction']}>
+               <label htmlFor='direction' className={styles['search-form__direction-label']}>
+                  Направление
+               </label>
+               <div className={styles['search-form__direction-container']}>
+                  <input
+                     id='direction'
+                     type='text'
+                     placeholder='откуда'
+                     className={styles['search-form__direction-input']}
+                  />
+                  <input
+                     type='text'
+                     placeholder='куда'
+                     className={styles['search-form__direction-input']}
+                  />
+               </div>
+            </div>
+            <div className={styles['search-form__date']}>
+               <label htmlFor='date' className={styles['search-form__date-label']}>
+                  Дата
+               </label>
+               <div className={styles['search-form__date-container']}>
+                  <input
+                     id='date'
+                     type='date'
+                     placeholder='дд/мм/гг'
+                     className={styles['search-form__date-input']}
+                  />
+                  <input
+                     type='date'
+                     placeholder='дд/мм/гг'
+                     className={styles['search-form__date-input']}
+                  />
+               </div>
+            </div>
+            <button className={styles['search-form__button']}>найти билеты</button>
+         </form>
       </main>
    );
 };
