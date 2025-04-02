807b0ae32eb6918224881bee78fa5aeee8d90aa6
Михаил Дорошин <demargorn@gmail.com>
2025-04-02 18:16:38 +0300

    config upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react()],
+   plugins: [
+      react(),
+      viteStaticCopy({
+         targets: [{ src: 'src/shared/assets/*', dest: 'assets' }],
+      }),
+   ],
    base: '/railway-sales_react-ts/',
+   publicDir: './src/app/shared',
    resolve: {
       alias: {
          '@': fileURLToPath(new URL('./src', import.meta.url)),
